672111617d4a60856daf47730b44f2fb97a4221a
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2023-08-16 16:41:26 -0300

    Ajuste Deploy

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'Portfolio_3d',
+  base: '/Portfolio_3d/',
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-     watch: {
-      usePolling: true
-     }
-  }
 })
