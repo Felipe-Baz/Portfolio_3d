857e6ea9e8f9810f65d7bdcf13d49d84a54b5f67
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2023-08-16 16:30:46 -0300

    feat: Build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '3d',
+  base: 'Portfolio_3d',
   plugins: [react()],
   server: {
     host: true,
